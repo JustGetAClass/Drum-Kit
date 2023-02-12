6b456f2494039c992e095b4fc7459857c8b5c2a1
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-12 13:02:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        this.style.color = "white";
+    });
+
+}
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
+
+
+// document.querySelectorAll(".drum").forEach(drum => {
+//     drum.addEventListener("click", function () {
+//         alert("clicked");
+//     })
+// });
